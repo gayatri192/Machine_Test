666278044670af1827ce4785bf0afe2006eda6bf
Gayatri Lohar <103237584+gayatri192@users.noreply.github.com>
2024-07-29 21:49:07 +0530

    Delete Machine_Test directory

--- Machine_Test/myapi/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.routers import DefaultRouter
-from .views import ClientViewSet, ProjectViewSet
-
-router = DefaultRouter()
-router.register(r'clients', ClientViewSet, basename='client')
-router.register(r'projects', ProjectViewSet, basename='project')
-
-urlpatterns = router.urls
